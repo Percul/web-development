50c90151d1e52f6f5656a9d2de7c7312913daabd
unknown <egwowaperculiar@gmail.com>
2022-06-10 12:08:03 +0100

    stopwatch/timer

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+//begin stopwatch program
+const stopwatchDisplay =document.querySelector('stopwatch-display')
+const stopwatchStartBtn = document.querySelector(' .btn-stopwatch-stop')
+const stopwatchstarttBtn = document.querySelector(' .btn-stopwatch-start')
+const stopwatchResetBtn = document.querySelector(' .btn-stopwatch-reset')
